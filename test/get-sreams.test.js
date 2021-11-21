b193ccf64b20fb83fdc210b5e1d9bfeee0ded607
Svetlana Tyshkevich <panfilova.ss@gmail.com>
2021-11-21 22:37:03 +0200

    refactor: delete comments

--- test/get-sreams.test.js
+++ test/get-sreams.test.js
@@ -6,18 +6,7 @@ test('length of array must be 3', () => {
   const config = 'C1-C0-A';
   expect(getStreams(config).length).toBe(3);
 });
-// test('should return encrypted string', () => {
-//   const string = 'This is secret. Message about "_" symbol!';
-//   const mockStdout = jest
-//     .spyOn(process, 'stdout')
-//     .mockImplementation(() => {});
-//   const config = 'C1-C1-R0-A';
-//   const transforms = getStreams(config);
-//   pipeline(string, ...transforms, mockStdout);
-//   expect(mockStdout).toHaveBeenCalledWith(
-//     'Myxn xn nbdobm. Tbnnfzb ferlm "_" nhteru!',
-//   );
-// });
+
 
 test('incorrect config must return process exit', () => {
   const mockExit = jest.spyOn(process, 'exit').mockImplementation(() => {});
